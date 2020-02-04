14f7abe74ed28e7843efd3f51336519423387dca
Benjamin <benjamin.martel.98@gmail.com>
2020-02-04 21:15:33 +0100

    Enemy AI following the player

---
+++ platformer-shooter-game/Assets/Scripts/EnemyAi.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyAi : MonoBehaviour
+{
+
+    public Transform target;
+
+    public float speed = 40f;
+    public float nextWaypointDistance = 3f;
+
+    public CharacterController2D controller;
+
+    Path path;
+    int currentWaypoint = 0;
+    bool reachedEndOfPath = false;
+
+    Seeker seeker;
+    Rigidbody2D rb;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("UpdatePath", 0f, 0.5f);
+    }
+
+    void UpdatePath()
+    {
+        if(seeker.IsDone())
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (path == null)
+            return;
+
+        if(currentWaypoint >= path.vectorPath.Count)
+        {
+            reachedEndOfPath = true;
+            return;
+        }
+        else
+        {
+            reachedEndOfPath = false;
+        }
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        Vector2 normalized = direction * speed * Time.deltaTime;
+
+
+        if (normalized.y > 0.8 )
+        {
+            jump = true;
+        }
+
+        if(normalized.x > 0.5)
+        {
+            horizontalMove = 1 * speed;
+        }
+        else if (normalized.x < -0.5)
+        {
+            horizontalMove = -1 * speed;
+        }
+        else
+        {
+            horizontalMove = 0;
+        }
+
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+        if(distance < nextWaypointDistance)
+        {
+            currentWaypoint++;
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
