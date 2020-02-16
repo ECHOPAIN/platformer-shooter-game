6d937c45bd354ed3cd3dc01618039abf84c30947
Benjamin <benjamin.martel.98@gmail.com>
2020-02-16 19:52:25 +0100

    Player reloading

--- platformer-shooter-game/Assets/Scripts/PlayerWeaponHandler.cs
+++ platformer-shooter-game/Assets/Scripts/PlayerWeaponHandler.cs
@@ -13,8 +13,24 @@ public class PlayerWeaponHandler : MonoBehaviour
     public Transform shotPoint;
     public Animator camAnim;
 
+    public bool infiniteAmo = false;
+    public int maxAmmo = 10;
+    private int currentAmmo;
+    public float reloadTime = 1f;
+
     private float timeBtwShots;
     public float startTimeBtwShots;
+    private bool isRealoading = false;
+
+    void Start()
+    {
+        currentAmmo = maxAmmo;
+    }
+
+    void OnEnable()
+    {
+        isRealoading = false;
+    }
 
     private void Update()
     {
@@ -23,11 +39,22 @@ public class PlayerWeaponHandler : MonoBehaviour
         float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
 
+        if (isRealoading)
+        {
+            return;
+        }
+        if(currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
         if (timeBtwShots <= 0)
         {
             if (Input.GetMouseButton(0))
             {
-                
+                if(!infiniteAmo)
+                    currentAmmo--;
                 //Destroy(Instantiate(shotEffect, shotPoint.position, shotPoint.rotation),0.1f);
                 //camAnim.SetTrigger("shake");
                 //CameraShaker.Instance.ShakeOnce(1f,0.1f,0f,.1f);
@@ -41,5 +68,13 @@ public class PlayerWeaponHandler : MonoBehaviour
         }
 
 
+    }
+
+    IEnumerator Reload()
+    {
+        isRealoading = true;
+        yield return new WaitForSeconds(reloadTime);
+        currentAmmo = maxAmmo;
+        isRealoading = false;
     }
 }
