ef0a29546eea13e046aecb27e629e8e93b94cb21
Benjamin <benjamin.martel.98@gmail.com>
2020-02-20 11:41:05 +0100

    Adding hammer to ennemy

---
+++ platformer-shooter-game/Assets/Scripts/Weapons/MeleeWeapon.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeWeapon : MonoBehaviour
+{
+
+    public float offset;
+
+    public Transform shotPoint;
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+    public float attackRange;
+    public LayerMask whatIsEnemies;
+    public int damage;
+
+    public LayerMask whatIsSolid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeBtwShots > 0)
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(shotPoint.position, attackRange);
+    }
+
+    public void shoot()
+    {
+        if (timeBtwShots <= 0)
+        {
+            Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(shotPoint.position, attackRange, whatIsEnemies);
+            for (int i = 0; i < enemiesToDamage.Length; i++)
+            {
+                if (enemiesToDamage[i] != gameObject)
+                    enemiesToDamage[i].GetComponent<HealthSystem>().TakeDamage(damage);
+            }
+            timeBtwShots = startTimeBtwShots;
+        }
+    }
+}
---
+++ platformer-shooter-game/Assets/Scripts/Weapons/MeleeWeaponHandlerPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeWeaponHandlerPlayer : MonoBehaviour
+{
+    public MeleeWeapon weapon;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO only rotate shotPoint not graphic
+
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + weapon.offset);
+
+        if (Input.GetMouseButton(0))
+            weapon.shoot();
+    }
+}
