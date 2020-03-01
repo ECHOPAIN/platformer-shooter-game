3657a2c2a871452f76603a405e1091e882e765cf
Benjamin <benjamin.martel.98@gmail.com>
2020-03-01 21:28:10 +0100

    WIP explosion

---
+++ platformer-shooter-game/Assets/ExplosiveProjectile.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosiveProjectile : MonoBehaviour
+{
+
+    public float speed;
+    public float lifeTime;
+    public float distance;
+    public int damage;
+    public float radius = 100f;
+    public float force = 700f;
+    public LayerMask whatIsSolid;
+
+    public GameObject destroyEffect;
+
+    private void Start()
+    {
+        Invoke("Explode", lifeTime);
+    }
+
+    private void Update()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.up, distance, whatIsSolid);
+        if (hitInfo.collider != null)
+        {
+            if (hitInfo.collider.CompareTag("Player"))
+            {
+                hitInfo.collider.GetComponent<HealthSystem>().TakeDamage(damage);
+            }
+            Explode();
+        }
+
+
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+
+    void Explode()
+    {
+        Instantiate(destroyEffect, transform.position, Quaternion.identity);
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, radius);
+
+        Debug.Log(colliders.Length);
+        
+        foreach(Collider2D nearbyObject in colliders)
+        {
+            Rigidbody2D rb = nearbyObject.GetComponent<Rigidbody2D>();
+            if(rb != null)
+            {
+                //rb.AddExplosionForce(force, transform.position, radius);
+            }
+
+            if (nearbyObject.CompareTag("Player"))
+            {
+                nearbyObject.GetComponent<HealthSystem>().TakeDamage(damage);
+            }
+        }
+
+        Destroy(gameObject);
+    }
+}
